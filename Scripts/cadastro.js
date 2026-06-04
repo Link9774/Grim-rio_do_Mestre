994347232bb0a11e4017d97550ad1f719dff98f3
Link9774 <pedrolink2003@hotmail.com>
2026-06-04 14:16:35 -0300

    Json inserido e cadastro funcional

--- Scripts/cadastro.js
+++ Scripts/cadastro.js
@@ -1,10 +1,10 @@
-const form = document.getElementById("registerForm");
+/*const form = document.getElementById("registerForm");
 
 form.addEventListener("submit", function(event){
     
     event.preventDefault();
 
-    //const userName = document.getElementById("userName").value;
+   // const userName = document.getElementById("userName");
     const savedUser = localStorage.getItem("userName");
 
     if(savedUser){
@@ -55,12 +55,104 @@ form.addEventListener("submit", function(event){
 
     if(valid){
         const userData = {
-            userName: userName,
-            email: userEmail,
-            password: password
+            userName: userName.value,
+            userEmail: userEmail.value,
+            password: password.value
         };
     }
-
+     fetch("http://localhost:3000/clientes",{
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+         .then(()=>{
+            alert("Usuário cadastrado");
+        }); 
 
 
 });
+*/
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const userName = document.getElementById("userName");
+    const userEmail = document.getElementById("userEmail");
+    const password = document.getElementById("password");
+    const confPassword = document.getElementById("confPassword");
+
+    const userNameError = document.getElementById("userNameError");
+    const userEmailError = document.getElementById("userEmailError");
+
+    let valid = true;
+
+    userName.classList.remove("error");
+    userNameError.textContent = "";   
+
+    if(userName.value.trim() === ""){
+        userName.classList.add("error")
+        userNameError.textContent = "O campo Nome de Usuário é obrigatório";
+        valid = false;
+    }
+     if(userName.value.length < 3){
+        userNameError.innerHTML = "O nome precisa ter pelo menos 3 caracteres.";
+
+        valid = false;
+    }
+    
+    userEmail.classList.remove("error"); 
+     userNameError.textContent = "";
+     
+     if(userEmail.value.trim() === ""){
+        userEmail.classList.add("error")
+        userEmailError.textContent = "O campo Email é obrigatório";
+        valid = false;
+    }
+    if(password.value == null){
+        alert("O campo Senha é obrigatorio")
+        valid = false;
+    }
+    
+    
+    if(password.value !== confPassword.value){
+        alert("As senhas não coincidem!");
+        valid = false;
+    }  
+    if(!userEmail.value.includes("@")){
+
+        userEmailError.innerHTML = "Digite um email válido.";
+
+        valid = false;
+    }
+
+    if(password.value.length < 6){
+        alert("A senha precisa ter no mínimo 6 caracteres.");
+
+        valid = false;
+    }
+    
+    
+    
+    if(valid){
+        const userData = {
+            userName: userName.value,
+            userEmail: userEmail.value,
+            password: password.value
+        };
+        fetch("http://localhost:3000/mestres",{
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers:{
+            "Content-Type": "application/json"
+        }
+        })
+         .then(()=>{
+            alert("Usuário cadastrado");
+        }); 
+    }
+    window.location.href = "login.html";
+
+})
