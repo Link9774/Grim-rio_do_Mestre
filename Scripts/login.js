14fd686f4f7d97e4a7d0d598026ccfb7b2b51e3c
Link9774 <pedrolink2003@hotmail.com>
2026-06-04 16:02:37 -0300

    Login funcional

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,26 +1,28 @@
 const loginForm = document.getElementById("loginForm");
 
-loginForm.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
 
     const email = document.getElementById("loginEmail").value;
-
     const password = document.getElementById("loginPassword").value;
+    const response = await fetch("http://localhost:3000/mestres");
+    const mestres = await response.json();
 
-
-    if(email !== "" && password !== ""){
-
-
-        localStorage.setItem("logado", "true");
-
-        window.location.href = dashboard.html;
-
+    console.log(mestres);
+console.log(loginEmail.value);
+console.log(loginPassword.value);
+    
+    const findMaster = mestres.find(
+        mestre => mestre.userEmail === loginEmail.value && mestre.password === loginPassword.value
+    );
+    if(findMaster){
+        console.log("Login realizado");
+        window.location.href = "dashboard.html";
     }else{
-
-        alert("Preencha todos os campos!");
-
+    
+        console.log("Email ou senha incorretos");
     }
 
 });
